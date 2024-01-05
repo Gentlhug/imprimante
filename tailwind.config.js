5e8993e3838cebf9d8717edc68109940fd1ebb7f
Hugo FAURIEL <75988412+Gentlhug@users.noreply.github.com>
2024-01-05 22:32:09 +0100

    Some updates

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'green1': '#EBF1CC',
+      'green2': '#BECB69',
+      'green3': '#9EA144',
+    },
+    fontFamily: {
+      clutsy: "Clutsy",
+      akaya: "Akaya Telivigala",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
